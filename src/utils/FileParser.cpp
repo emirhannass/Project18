0cc47232cc436a1d71271c56fd6ac99064eb2df9
Emirhan <emirhannas.cse@gmail.com>
2026-05-27 16:17:14 +0300

    artık datadaki json dosyasından gerçek zqamanlı veri çekiyoruz

--- src/utils/FileParser.cpp
+++ src/utils/FileParser.cpp
@@ -1,68 +1,134 @@
 /**
  * @file FileParser.cpp
- * @brief Örnek girdi verilerinin el ile işlenerek sisteme doldurulması.
+ * @brief Girdi JSON verilerinin dinamik olarak işlenerek sisteme doldurulması.
  * @author Proje Grubu
  * @date 2026-05-26
  */
 
 #include "utils/FileParser.h"
 #include <iostream>
+#include <fstream>
+#include <string>
 
 namespace utils {
 
+// Yardımcı Fonksiyon: Satır içerisindeki belirli bir JSON anahtarının (key) değerini ayıklar
+std::string extractValue(const std::string& line, const std::string& key) {
+    size_t keyPos = line.find("\"" + key + "\"");
+    if (keyPos == std::string::npos) return "";
+
+    size_t colonPos = line.find(":", keyPos);
+    if (colonPos == std::string::npos) return "";
+
+    // Değerin başlangıç noktasını bul (boşlukları atla)
+    size_t valueStart = colonPos + 1;
+    while (valueStart < line.size() && (line[valueStart] == ' ' || line[valueStart] == '\t')) {
+        valueStart++;
+    }
+
+    if (valueStart < line.size() && line[valueStart] == '"') {
+        // Metinsel (String) değerler için iki tırnak arasını al
+        size_t valueEnd = line.find("\"", valueStart + 1);
+        if (valueEnd != std::string::npos) {
+            return line.substr(valueStart + 1, valueEnd - valueStart - 1);
+        }
+    } else {
+        // Sayısal (Integer) değerler için (Örn: seniority) rakamları topla
+        std::string numStr = "";
+        while (valueStart < line.size() && (std::isdigit(line[valueStart]) || line[valueStart] == '-')) {
+            numStr += line[valueStart];
+            valueStart++;
+        }
+        return numStr;
+    }
+    return "";
+}
+
 bool FileParser::loadMockData(core::CrewRosterManager& manager, const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "[HATA] JSON dosyasi acilamadi: " << filename << "\n";
+        return false;
+    }
+
     std::cout << "[INFO] Girdi verileri yukleniyor: " << filename << "\n";
 
-    // PROJ-18 ucus verileri ekleniyor
-    core::Flight f1;
-    f1.id = "FL100"; f1.from_airport = "IST"; f1.to_airport = "ESB"; 
-    f1.depart_time = "2026-04-01T08:00"; f1.arrive_time = "2026-04-01T09:15"; f1.aircraft_type = "A320";
+    std::string line;
+    bool inFlights = false;
+    bool inCrew = false;
 
-    core::Flight f2;
-    f2.id = "FL101"; f2.from_airport = "ESB"; f2.to_airport = "ADB"; 
-    f2.depart_time = "2026-04-01T10:30"; f2.arrive_time = "2026-04-01T11:45"; f2.aircraft_type = "A320";
+    while (std::getline(file, line)) {
+        // Hangi JSON dizisinde (array) olduğumuzu tespit etme
+        if (line.find("\"flights\"") != std::string::npos) {
+            inFlights = true;
+            inCrew = false;
+            continue;
+        }
+        if (line.find("\"crew\"") != std::string::npos) {
+            inFlights = false;
+            inCrew = true;
+            continue;
+        }
+        if (line.find("\"regulations\"") != std::string::npos) {
+            inFlights = false;
+            inCrew = false;
+            continue;
+        }
 
-    core::Flight f3;
-    f3.id = "FL102"; f3.from_airport = "ADB"; f3.to_airport = "IST"; 
-    f3.depart_time = "2026-04-01T14:00"; f3.arrive_time = "2026-04-01T15:20"; f3.aircraft_type = "B737";
+        // 1. UÇUŞ SATIRLARINI AYRIŞTIRMA
+        if (inFlights && line.find("{\"id\"") != std::string::npos) {
+            core::Flight f;
+            f.id = extractValue(line, "id");
+            f.from_airport = extractValue(line, "from");
+            f.to_airport = extractValue(line, "to");
+            f.depart_time = extractValue(line, "depart");
+            f.arrive_time = extractValue(line, "arrive");
+            f.aircraft_type = extractValue(line, "aircraft");
 
-    core::Flight f4;
-    f4.id = "FL103"; f4.from_airport = "IST"; f4.to_airport = "ADB"; 
-    f4.depart_time = "2026-04-02T07:00"; f4.arrive_time = "2026-04-02T08:20"; f4.aircraft_type = "A320";
-    
-    manager.addFlight(f1);
-    manager.addFlight(f2);
-    manager.addFlight(f3);
-    manager.addFlight(f4);
+            if (!f.id.empty()) {
+                manager.addFlight(f);
+            }
+        }
 
-    // Ekip nesneleri olusturuluyor (Constructor hatasi giderildi)
-    core::Crew c1;
-    c1.id = "CR01"; 
-    c1.role = "captain"; 
-    c1.base = "IST"; 
-    c1.seniority = 15;
-    c1.qualifications.add("A320");
-    c1.qualifications.add("B737");
+        // 2. EKİP SATIRLARINI AYRIŞTIRMA
+        if (inCrew && line.find("{\"id\"") != std::string::npos) {
+            core::Crew c;
+            c.id = extractValue(line, "id");
+            c.role = extractValue(line, "role");
+            c.base = extractValue(line, "base");
+            
+            std::string senStr = extractValue(line, "seniority");
+            if (!senStr.empty()) {
+                c.seniority = std::stoi(senStr);
+            }
 
-    core::Crew c2;
-    c2.id = "CR02"; 
-    c2.role = "captain"; 
-    c2.base = "IST"; 
-    c2.seniority = 8;
-    c2.qualifications.add("A320");
+            // Yetkinlikleri (qualifications: ["A320", "B737"]) ayıklama döngüsü
+            size_t startBracket = line.find("[");
+            size_t endBracket = line.find("]");
+            if (startBracket != std::string::npos && endBracket != std::string::npos) {
+                std::string qualsRaw = line.substr(startBracket + 1, endBracket - startBracket - 1);
+                size_t qPos = 0;
+                while ((qPos = qualsRaw.find("\"", qPos)) != std::string::npos) {
+                    size_t nextQ = qualsRaw.find("\"", qPos + 1);
+                    if (nextQ != std::string::npos) {
+                        std::string qual = qualsRaw.substr(qPos + 1, nextQ - qPos - 1);
+                        if (!qual.empty()) {
+                            c.qualifications.add(qual);
+                        }
+                        qPos = nextQ + 1;
+                    } else {
+                        break;
+                    }
+                }
+            }
 
-    core::Crew c3;
-    c3.id = "CR03"; 
-    c3.role = "first_officer"; 
-    c3.base = "ESB"; 
-    c3.seniority = 5;
-    c3.qualifications.add("A320");
-    c3.qualifications.add("B737");
+            if (!c.id.empty()) {
+                manager.addCrew(c);
+            }
+        }
+    }
 
-    manager.addCrew(c1);
-    manager.addCrew(c2);
-    manager.addCrew(c3);
-
+    file.close();
     return true;
 }
 
